d1f8db9dae80133bb20d903516fe03228dd2e25e
Unknown <alyonabill3@mail.ru>
2017-12-17 14:36:12 +0300

    The button "Done"has finished. Now when the page is loaded, the state of the button remains.

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -14,11 +14,11 @@ export class TaskListComponent {
   oneDone() {
     if(this.item.done) {
       this.item.done = false
-      document.getElementById('button_done').innerHTML = "Done"
+      // document.getElementById('button_done').innerHTML = "Done"
     }
     else {
       this.item.done = true
-      document.getElementById('button_done').innerHTML = "Cancel"
+      // document.getElementById('button_done').innerHTML = "Cancel"
     }
     localStorage.setItem(this.item.id.toString(), JSON.stringify(this.item))
   }
